bba4e7dce108a9843223d4def92360a5c1afa436
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-28 13:07:39 +0900

    deviseのflashメッセージの日本語化

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.javascripts false
       g.helper false
