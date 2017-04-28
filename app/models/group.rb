182af9321863031e66220aa45bafbaa8ddfd99a4
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-28 21:24:41 +0900

    アソシエーションの設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  has_many :users, through: :group_users
+  has_many :group_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :groups, through: :group_users
+  has_many :group_users
 end
