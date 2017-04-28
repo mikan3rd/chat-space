c3a7cb5b8366ce73414c6437b080506b84e32a31
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-28 18:36:41 +0900

    軽微な修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
+private
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-def after_sign_in_path_for(resource)
-  root_path
-end
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
-def after_sign_out_path_for(resource)
-  new_user_session_path
-end
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
