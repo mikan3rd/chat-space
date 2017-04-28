4a1f367495d06dce4b0a3e874c1cf76436b2491f
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-28 13:43:02 +0900

    サインイン・アウト後のルートの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+def after_sign_in_path_for(resource)
+  rnew_user_session_path
+end
+
+def after_sign_out_path_for(resource)
+  admin_root_path
+end
+
   private
   before_action :configure_permitted_parameters, if: :devise_controller?
 
