b30d9b41d97a30376e53a25924815c32dd3b7903
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-28 13:45:37 +0900

    サインイン・アウト後のルートの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 def after_sign_in_path_for(resource)
-  rnew_user_session_path
+  root_path
 end
 
 def after_sign_out_path_for(resource)
-  admin_root_path
+  new_user_session_path
 end
 
   private
