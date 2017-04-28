26ec69836186bd7406791409073c11cf99889b43
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-28 17:35:07 +0900

    flashの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ def after_sign_out_path_for(resource)
   new_user_session_path
 end
 
-  private
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
