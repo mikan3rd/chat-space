46a6fb2991d91b67104c76acb125aace9ec1099e
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-29 20:45:34 +0900

    グループメッセージ画面へのリンク設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
   def index
+    @groups = Group.order(id: :DESC)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'groups#index'
   devise_for :users
-  resources :groups, except: [:destroy, :show]
+  resources :groups, except: [:destroy, :show] do
+    resources :messages, only: [:index]
+  end
 end
