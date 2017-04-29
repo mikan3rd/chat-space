9ba6da48797baa0fb558e2f12a11f7025c9cb4fc
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-29 19:54:52 +0900

    指摘された箇所の修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groups = Group.order("updated_at DESC")
+    @groups = Group.order(id: "DESC")
   end
 
   def new
@@ -10,11 +10,11 @@ class GroupsController < ApplicationController
   end
 
   def create
-    group = Group.create(group_params)
-
-    if group.name == ""
+    group = Group.new(group_params)
+    unless group.save
       flash[:alert] = "グループ名を入力してください"
-      redirect_to "/groups/new"
+      @group = group
+      render :new
     else
       redirect_to root_path
     end
@@ -27,8 +27,8 @@ class GroupsController < ApplicationController
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
-
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,6 @@ class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users
   accepts_nested_attributes_for :group_users, allow_destroy: true
+
   validates :name, presence: true
 end
