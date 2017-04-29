c03620b7fd3e9e2097fa67e411183ee9227345b5
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-29 12:50:41 +0900

    グループ名送信機能の設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,18 +1,28 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
+
   def index
   end
 
   def new
+    @group = Group.new
   end
 
   def create
+    Group.create(group_params)
+    redirect_to root_path
+
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
   end
 
 end
