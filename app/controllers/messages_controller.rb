432d89a8439d03341de59c87a6179865be36dc92
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-29 21:26:42 +0900

    グループメッセージ画面の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,5 +2,6 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
   def index
     @groups = Group.order(id: :DESC)
+    @group = Group.find(params[:group_id])
   end
 end
