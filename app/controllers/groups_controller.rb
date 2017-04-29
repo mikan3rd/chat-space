e6d97028123e1b95510c430e1eb0710c5f0fc7c6
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-04-29 22:04:06 +0900

    グループ編集機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,14 +16,25 @@ class GroupsController < ApplicationController
       @group = group
       render :new
     else
+      flash[:notice] = "グループを作成しました"
       redirect_to root_path
     end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    group = Group.find(params[:id])
+    if group.update(group_params) == false
+      flash[:alert] = "グループ名を入力してください"
+      @group = group
+      render :edit
+    else
+      flash[:notice] = "グループを編集しました"
+      redirect_to "/groups/#{group.id}/messages"
+    end
   end
 
   private
