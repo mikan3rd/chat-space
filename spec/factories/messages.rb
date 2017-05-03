5504f5bb52526e4b3e848839295901b7a7c9a6ca
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-03 14:18:01 +0900

    factory_girlの設定

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,8 +2,19 @@ FactoryGirl.define do
 
   factory :message do
     body { Faker::Lorem.sentence }
-    group_id 1
-    user_id 1
+    group_id { Faker::Number.number(2)}
+    user_id { Faker::Number.number(2)}
+    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 
+  factory :group do
+    name { Faker::Pokemon.location }
+    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
+  end
+
+  factory :user do
+    name { Faker::Pokemon.name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(8, 10)}
+  end
 end
