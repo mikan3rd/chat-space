1a5c7d1f779baf032ab9ef6ad243e0c042c1aa94
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-03 18:22:36 +0900

    テスト項目の定義

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -19,11 +19,23 @@ describe MessagesController do
       expect(assigns(:groups)).to match(groups.sort{ |a, b| b.created_at <=> a.created_at })
     end
 
-    # it "renders the :index template" do
+    it "@groupの確認" do
+    end
+
+    it "@usersの確認" do
+    end
+
+    it "@messagesの確認" do
+    end
+
+    it "@messageの確認" do
+    end
+
+    it "ビューに移動するかの確認" do
     #   group = create(:group)
     #   user = create_list(:user, 3)
     #   get :index, group_id: group
     #   expect(response).to render_template :index
-    # end
+    end
   end
 end
