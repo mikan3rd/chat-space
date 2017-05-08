0cb43f8837e939dab6691c071e0c0d1729a65e7c
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-08 21:10:30 +0900

    ユーザー削除機能の実装

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -6,7 +6,7 @@ $(document).on('turbolinks:load', function() {
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">
           ${user.name}</p>
-        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=" ${user.id}" data-user-name=" ${user.name} ">追加
+        <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
         </a>
       </div>`;
     return html
@@ -15,7 +15,7 @@ $(document).on('turbolinks:load', function() {
 // ユーザー追加ボタンクリック時に追加されるHTMLの生成
   function buildMemberHTML(id, name) {
     var html = `
-    <div class="chat-group-user clearfix">
+    <div class="chat-group-user clearfix" id=chat-group-user-${id}>
       <input type="hidden" name="group[user_ids][]" value="${id}">
       <p class="chat-group-user__name">${name}</p>
       <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}">削除</a>
@@ -55,4 +55,9 @@ $(document).on('turbolinks:load', function() {
     $('#chat-group-users').append(insertHTML);
     $(this).parent('.chat-group-user').remove();
   });
+  // メンバーの削除
+  $('#chat-group-users').on('click', '.user-search-remove', function() {
+    var id = $(this).data('userId');
+    $(`#chat-group-user-${id}`).remove();
+  })
 });
