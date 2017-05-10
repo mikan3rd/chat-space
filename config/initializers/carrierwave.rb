04acc155dcfa9e368337dd9b379177d4c725bbf5
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-10 10:55:48 +0900

    carrierwabe.rbの修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,9 @@ Dotenv.load
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: '',
-    aws_secret_access_key: '',
-    region: 'ap-northeast-1'
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION']
   }
-  config.fog_directory = 'mikan3rd-chat-space'
+  config.fog_directory = ENV['AWS_BUCKET_NAME']
 end
