182dd196b7b40e90aff06b170b99af93bc6d0b22
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-12 12:14:09 +0900

    差分のみ表示を追加するよう設定

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -7,7 +7,7 @@ $(document).on('turbolinks:load', function() {
       insertImage = `<img src="${message.image.url}">`;
     }
     var html = `
-      <div class="chat">
+      <div class="chat" data-message-id="${message.id}">
         <p class="chat__user">${message.name}</p>
         <p class="chat__date">${message.date}</p>
         <p class="chat__content">${message.body}</p>
@@ -42,7 +42,7 @@ $(document).on('turbolinks:load', function() {
   });
 
   // メッセージ自動更新
-    var id = setInterval(function() {
+    var interval = setInterval(function() {
       if (window.location.href.match(/\/groups\/\d+\/messages/)) {
     $.ajax({
       type: 'GET',
@@ -50,17 +50,21 @@ $(document).on('turbolinks:load', function() {
       dataType: 'json'
     })
     .done(function(data) {
+      var id = $('.chat').data('messageId');
+      console.log( "最新のメッセージのid：" + id);
       var insertHTML = '';
       data.messages.forEach(function(message) {
-        insertHTML += buildHTML(message);
+        if (message.id > id ) {
+          insertHTML += buildHTML(message);
+        }
       });
-      $('.chat-wrapper').html(insertHTML);
+      $('.chat-wrapper').prepend(insertHTML);
       console.log('自動更新に成功しました！');
     })
     .fail(function(data) {
-      alert('自動更新に失敗しました');
+      console.log('自動更新に失敗しました');
     });
   } else {
-    clearInterval(id);
+    clearInterval(interval);
    }} , 5 * 1000 );
 });
--- app/views/messages/index.json.jbuilder
+++ app/views/messages/index.json.jbuilder
@@ -3,4 +3,5 @@ json.messages @messages.each do |message|
   json.date     message.created_at.strftime("%Y年%m月%d日 %H時%M分")
   json.body     message.body
   json.image    message.image
+  json.id       message.id
 end
